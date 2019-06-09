ae528114aa87307019ee9d3a4a4b83c6cb6885c2
kangtaesung <congoha@gmail.com>
2019-06-09 13:32:40 +0900

    Initial commit

---
+++ EmployeeManagerBrowser/WebContent/EmployeeManager.jsp
@@ -0,0 +1,20 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+	<h1>Edited from the other workspace</h1>
+	<form action="EmployeeManagerServlet" method="post">
+		Id: <input type="text" name="id" size="20"><br>
+		<br> Age: <input type="text" name="age" size="20"><br>
+		<br> Name: <input type="text" name="name" size="20"><br>
+		<br>access Access Level: <input type="text" name="access"
+			size="20"><br>
+		<br> <input type="submit" value="add Employee" />
+	</form>
+</body>
+</html>
---
+++ EmployeeManagerBrowser/src/com/barosanu/web/EmployeeManagerServlet.java
@@ -0,0 +1,32 @@
+package com.barosanu.web;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class EmployeeManagerServlet
+ */
+@WebServlet("/EmployeeManagerServlet")
+public class EmployeeManagerServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public EmployeeManagerServlet() {
+        super();
+        // TODO Auto-generated constructor stubss
+    }
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
